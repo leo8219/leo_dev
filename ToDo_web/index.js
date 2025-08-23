2780667f7a4d255f2db0e0324788e5e77a3fd624
김용태 <kytkindom@naver.com>
2025-08-23 15:38:06 +0900

    todo작업 추가기능 구현

--- ToDo_web/index.js
+++ ToDo_web/index.js
@@ -1,3 +1,16 @@
-// Created on iPad.
+const todoForm = document.getElementById('todobox');
+const todoInput = document.getElementById('todo-txt');
+const todoList = document.getElementById('todo-list');
+const todoTemplate = document.getElementById('todo-items')
 
-console.log("Hello, World!")
+const formSubmit = (event) => {
+    event.preventDefault();
+    const inputValue = todoInput.value.trim();
+    if(inputValue==='') return;
+    const todoItem = todoTemplate.content.cloneNode(true).querySelector('.todo-item');
+
+    todoItem.querySelector('.item-title').textContent = inputValue;
+    todoList.appendChild(todoItem);
+    todoInput.value='';
+};
+todoForm.addEventListener('submit', formSubmit);
